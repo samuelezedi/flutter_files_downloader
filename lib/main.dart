faf1fe6723882c300446010ceea27c76addd8fd0
Samuel Ezedi <samuelezedi@gmail.com>
2020-04-17 22:38:40 +0100

    progress in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
 
 void main() => runApp(MyApp());
 
@@ -7,17 +10,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
+
         primarySwatch: Colors.blue,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
@@ -28,14 +24,7 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -44,68 +33,131 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
+  TextEditingController url = TextEditingController();
+  TextEditingController fileName = TextEditingController();
+  TextEditingController fileType = TextEditingController();
+  GlobalKey formKey = GlobalKey();
+
+  bool downloading = false;
+  String progressString = "";
+
+  Future<void> downloadFile() async {
+    Dio dio = Dio();
+
+    try{
+      var dir = await getApplicationDocumentsDirectory();
+
+      await dio.download(url.text, "${dir.path}/downloads/${fileName}", onReceiveProgress: (rec,total){
+        print("Rec: $rec, total: $total");
+        setState(() {
+          downloading = true;
+          progressString = ((rec/total) * 100).toStringAsFixed(0) + " %";
+        });
+      });
+
+    }catch(e) {
+      print(e);
+    }
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      downloading = false;
+      progressString = "";
     });
   }
 
   @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('File Downloader'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
+      body: Container(
+        margin: EdgeInsets.all(20),
+        child: Form(
+          key: formKey,
+          child: Column(
+            children: <Widget>[
+              Text('Enter link below'),
+                TextFormField(
+                  controller: url,
+                  keyboardType: TextInputType.url,
+                  validator: (value){
+                    if(value.trim() == ""){
+                      return 'Please enter a valid url';
+                    } else {
+                      return null;
+                    }
+                  },
+                  decoration: InputDecoration(
+                    labelText: 'Link',
+                    hintText: 'Enter link'
+                  ),
+
+              ),
+              TextFormField(
+                controller: fileName,
+                keyboardType: TextInputType.text,
+                validator: (value){
+                  if(value.trim() == ""){
+                    return 'Please enter a name';
+                  } else {
+                    return null;
+                  }
+                },
+                decoration: InputDecoration(
+                    labelText: 'File name',
+                    hintText: 'Enter name'
+                ),
+
+              ),
+              TextFormField(
+                controller: fileType,
+                keyboardType: TextInputType.text,
+                validator: (value){
+                  if(value.trim() == "" || value.trim() != 'jpeg' || value.trim() != 'png' || value.trim() != 'jpg'){
+                    return 'Please enter a valid file type';
+                  } else {
+                    return null;
+                  }
+                },
+                decoration: InputDecoration(
+                    labelText: 'Type',
+                    hintText: 'accepts. jpg, pdf, jpeg'
+                ),
+
+              )
+            ],
+          ),
+        )
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
+        onPressed: (){
+
+        },
+        tooltip: 'Download file',
+        child: Icon(Icons.file_download),
       ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  loading() {
+    return showDialog(
+        context: context,
+        child: AlertDialog(
+          title: Row(
+            children: <Widget>[
+              CircularProgressIndicator(),
+              Text('Downloading file...')
+            ],
+          ),
+        )
     );
   }
 }
